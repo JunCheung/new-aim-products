e44ecc4a58409338fe85ed55bbbb6f198b4794e9
Jun Cheung <juncheung90@163.com>
2024-03-17 19:33:32 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  //vue.config.js
+  css: {
+    loaderOptions: {
+      postcss: {
+        postcssOptions: {
+          plugins: [
+            require('tailwindcss'),
+            require('autoprefixer')
+          ]
+        }
+      }
+    }
+  }
+
+})
